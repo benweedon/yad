1d3be85030c16d93984b36e491ca0ed5b0701c87
Ben Weedon <ben.weedon@outlook.com>
2018-03-13 22:54:06 +0300

    Make code more unit testable
    Separate out functionality into smaller functions.

--- addon/popups.js
+++ addon/popups.js
@@ -1,44 +1,62 @@
-let walk = document.createTreeWalker(
-    document.body,
-    NodeFilter.SHOW_TEXT,
-    {
-        acceptNode: function(node) {
-            if (node.nodeValue.trim().length == 0 ||
-                ['SCRIPT', 'STYLE'].includes(node.parentNode.tagName)) {
-                return NodeFilter.FILTER_SKIP;
-            } else {
-                return NodeFilter.FILTER_ACCEPT;
+const YIDDISH_REGEX = /(?:^|\s)\S{3}(?=$|\s)/;
+
+function getTextNodes() {
+    let walk = document.createTreeWalker(
+        document.body,
+        NodeFilter.SHOW_TEXT,
+        {
+            acceptNode: function(node) {
+                if (node.nodeValue.trim().length == 0 ||
+                    ['SCRIPT', 'STYLE'].includes(node.parentNode.tagName)) {
+                    return NodeFilter.FILTER_SKIP;
+                } else {
+                    return NodeFilter.FILTER_ACCEPT;
+                }
             }
-        }
-    },
-    false
-);
-let newNodes = [];
-let node;
-while (node = walk.nextNode()) {
-    let val = node.nodeValue;
-    let toAdd = [];
+        },
+        false
+    );
+    let nodes = [];
+    let node;
+    while (node = walk.nextNode()) {
+        nodes.push(node);
+    }
+    return nodes;
+}
+
+function produceNewNodes(val, regex, f) {
+    let nodes = [];
     let result;
-    while (result = val.match(/(?:^|\s)\S{3}(?=$|\s)/)) {
+    while (result = val.match(regex)) {
         let addend = val[result.index].match(/\s/) ? 1 : 0;
         let preceding = val.substring(0, result.index + addend);
         if (preceding.length != 0) {
-            toAdd.push(document.createTextNode(preceding));
+            nodes.push(document.createTextNode(preceding));
             val = val.substring(result.index + addend);
         }
-        let underlined = document.createElement("u");
-        underlined.appendChild(document.createTextNode(result[0].substring(addend)));
-        toAdd.push(underlined);
+        nodes.push(f(result[0].substring(addend)));
         val = val.substring(result[0].length - addend);
     }
     if (val.length != 0) {
-        toAdd.push(document.createTextNode(val));
+        nodes.push(document.createTextNode(val));
     }
-    newNodes.push([node, toAdd]);
+    return nodes;
 }
-for (let [node, sibs] of newNodes) {
-    for (let newNode of sibs) {
+
+function underline(text) {
+    let underlined = document.createElement("u");
+    underlined.appendChild(document.createTextNode(text));
+    return underlined;
+}
+
+function replaceWithNodes(node, newNodes) {
+    for (let newNode of newNodes) {
         node.parentNode.insertBefore(newNode, node);
     }
     node.parentNode.removeChild(node);
+}
+
+for (let node of getTextNodes()) {
+    let newNodes = produceNewNodes(node.nodeValue, YIDDISH_REGEX, underline);
+    replaceWithNodes(node, newNodes);
 }
