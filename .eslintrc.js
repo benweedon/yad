dca9cb29ac1f5c915828c980f703d27c5cb2420d
Ben Weedon <ben.weedon@outlook.com>
2018-03-19 18:44:00 -0700

    Use eslint as part of linting script

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "browser": true,
+        "es6": true,
+        "mocha": true,
+        "node": true
+    },
+    "plugins": [
+        "json",
+        "mocha",
+        "promise"
+    ],
+    "rules": {
+        "no-cond-assign": "off",
+        "quotes": ["error", "single"]
+    }
+};
