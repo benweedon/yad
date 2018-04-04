c3ea1c03543cd2c5b86b6f78ea0780391a53a218
Ben Weedon <ben.weedon@outlook.com>
2018-04-03 22:06:06 -0700

    Get minimized version of dictionary from Github Releases
    It's about 39% the size, which is significant.

--- addon/background.js
+++ addon/background.js
@@ -6,14 +6,30 @@
 
 var dict = null;
 
-let xmlHttp = new XMLHttpRequest();
-xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-        dict = JSON.parse(xmlHttp.responseText);
+let latestReleaseRequest = new XMLHttpRequest();
+latestReleaseRequest.addEventListener('readystatechange', function() {
+    if (latestReleaseRequest.readyState == 4 && latestReleaseRequest.status == 200) {
+        let response = JSON.parse(latestReleaseRequest.responseText)
+        let assetUrl;
+        for (let asset of response.assets) {
+            if (asset.name == 'min.json') {
+                assetUrl = asset.url;
+            }
+        }
+
+        let dictRequest = new XMLHttpRequest();
+        dictRequest.addEventListener('readystatechange', function() {
+            if (dictRequest.readyState == 4 && dictRequest.status == 200) {
+                dict = JSON.parse(dictRequest.responseText);
+            }
+        });
+        dictRequest.open('GET', assetUrl, true);
+        dictRequest.setRequestHeader('Accept', 'application/octet-stream');
+        dictRequest.send(null);
     }
-}
-xmlHttp.open('GET', 'https://raw.githubusercontent.com/benweedon/yiddish_dictionary/master/dict.json', true);
-xmlHttp.send(null);
+});
+latestReleaseRequest.open('GET', 'https://api.github.com/repos/benweedon/yiddish_dictionary/releases/latest', true);
+latestReleaseRequest.send(null);
 
 browser.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg in dict) {
