7bfb2421500967cbccd491bdc61f58824908e4d7
Ben Weedon <ben.weedon@outlook.com>
2018-04-05 14:37:39 -0700

    No longer pull min.json from Releases, instead Pages
    This requires one fewer web request, and also is not rate limited.

--- addon/background.js
+++ addon/background.js
@@ -6,30 +6,14 @@
 
 var dict = null;
 
-let latestReleaseRequest = new XMLHttpRequest();
-latestReleaseRequest.addEventListener('readystatechange', function() {
-    if (latestReleaseRequest.readyState == 4 && latestReleaseRequest.status == 200) {
-        let response = JSON.parse(latestReleaseRequest.responseText)
-        let assetUrl;
-        for (let asset of response.assets) {
-            if (asset.name == 'min.json') {
-                assetUrl = asset.url;
-            }
-        }
-
-        let dictRequest = new XMLHttpRequest();
-        dictRequest.addEventListener('readystatechange', function() {
-            if (dictRequest.readyState == 4 && dictRequest.status == 200) {
-                dict = JSON.parse(dictRequest.responseText);
-            }
-        });
-        dictRequest.open('GET', assetUrl, true);
-        dictRequest.setRequestHeader('Accept', 'application/octet-stream');
-        dictRequest.send(null);
+let minDictRequest = new XMLHttpRequest();
+minDictRequest.addEventListener('readystatechange', function() {
+    if (minDictRequest.readyState == 4 && minDictRequest.status == 200) {
+        dict = JSON.parse(minDictRequest.responseText)
     }
 });
-latestReleaseRequest.open('GET', 'https://api.github.com/repos/benweedon/yiddish_dictionary/releases/latest', true);
-latestReleaseRequest.send(null);
+minDictRequest.open('GET', 'https://benweedon.github.io/yiddish_dictionary/min.json', true);
+minDictRequest.send(null);
 
 function handleMessage(msg, sender, sendResponse) {
     if (dict != null) {
